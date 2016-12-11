a60eed59eb146d05ab7ea2d875fb4ab5f441436c
MertEgeCAN <m.egecan@hotmail.com>
2016-12-11 20:56:56 +0300

    Testing git for King class changes
    Test upload

--- BattleChess/src/GameEntities/Pieces/King.java
+++ BattleChess/src/GameEntities/Pieces/King.java
@@ -4,4 +4,13 @@ package GameEntities.Pieces;
 
  */
 public class King extends  Piece{
+
+    @Override
+    public boolean isValid(int curX, int curY, int tarX, int tarY) {
+        if(!super.isValid(curX, curY, tarX, tarY))
+            return false;
+        else if (Math.abs(curX - tarX) > 1 || Math.abs(curY - tarY) > 1)
+            return false;
+        return true;
+    }
 }
