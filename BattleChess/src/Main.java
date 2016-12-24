6733cc63ed9ebcd1c23aa78a6fd4ad7f359467be
AbdullahWali <aboudwali@gmail.com>
2016-12-24 17:33:41 +0300

    Added Name and Icon

--- BattleChess/src/Main.java
+++ BattleChess/src/Main.java
@@ -1,10 +1,14 @@
 import UserInterface.GameFrame;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
 
         GameFrame frame = new GameFrame();
+        frame.setTitle("Chess++");
+        frame.setIconImage(new ImageIcon(Main.class.getClass().getResource("/assets/chess.png")).getImage());
 
     }
 }
